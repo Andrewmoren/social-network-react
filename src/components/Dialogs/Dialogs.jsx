a5b6461a866957a32a64d6ea17c2fdd688a06eca
andrew <andrewmoren93@gmail.com>
2022-08-03 12:00:42 +0300

    add props dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,30 @@
 import d from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+
+  return (
+    <div className={d.dialog + " " + d.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
 
 const Dialogs = (prop) => {
   return (
     <div className={d.dialogs}>
       <div className={d.dialogsItems}>
-        <div className={d.dialog + " " + d.active}>Andrew</div>
-        <div className={d.dialog}>Loli</div>
-        <div className={d.dialog}>Alex</div>
-        <div className={d.dialog}>Antony</div>
+        <DialogItem name="Andrew" id="1" />
+        <div className={d.dialog}>
+          <NavLink to="/dialogs/2">Loli</NavLink>
+        </div>
+        <div className={d.dialog}>
+          <NavLink to="/dialogs/3">Alex</NavLink>
+        </div>
+        <div className={d.dialog}>
+          <NavLink to="/dialogs/4">Antony</NavLink>
+        </div>
       </div>
       <div className={d.messages}>
         <div className={d.message}>Hi</div>
