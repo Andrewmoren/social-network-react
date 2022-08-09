2a92f9d5d965dcb7e8283efaed740c0d6858deb8
andrew <andrewmoren93@gmail.com>
2022-08-09 23:56:02 +0300

    add method dispatch 2

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ const App = (props) => {
               element={
                 <Profile
                   profilePage={props.state.profilePage}
-                  addPost={props.addPost}
-                  updateNewPostText={props.updateNewPostText}
+                  dispatch={props.dispatch}
                 />
               }
             />
