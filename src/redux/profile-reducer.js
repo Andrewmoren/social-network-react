98f77b66ffdb189b8ec2c00d825749881c8889e3
andrew <andrewmoren93@gmail.com>
2022-08-16 15:47:20 +0300

    add prodile- deducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,3 +1,18 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 const profileReducer = (state, action) => {
+  if (action.type === "ADD-POST") {
+    let newPost = {
+      id: 4,
+      post: this._state.profilePage.newPostText,
+      likecount: 0,
+    };
+    state.posts.push(newPost);
+    state.newPostText = "";
+  } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+    state.newPostText = action.newText;
+  }
+
   return state;
 };
