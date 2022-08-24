aae599fdc1d77928214eac0a9e77f53dd9a8f3e8
andrew <andrewmoren93@gmail.com>
2022-08-24 22:28:08 +0300

    fix dialogs container component 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import Music from "./components/Music/Music";
