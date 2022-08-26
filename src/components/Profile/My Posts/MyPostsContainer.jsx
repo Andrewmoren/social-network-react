4b4c13d89df809559f0735c898170fa3dd63c988
andrew <andrewmoren93@gmail.com>
2022-08-26 14:57:01 +0300

    fix store context api 2

--- src/components/Profile/My Posts/MyPostsContainer.jsx
+++ src/components/Profile/My Posts/MyPostsContainer.jsx
@@ -7,27 +7,28 @@ import StoreContext from "../../../storeContext";
 import MyPost from "./MyPosts";
 
 const MyPostsContainer = (props) => {
-  // let state = props.store.getState();
-
-  let addPost = () => {
-    props.store.dispatch(addPostActionCreator());
-  };
-
-  let onPostChange = (text) => {
-    let action = updateNewPostTextActionCreator(text);
-    props.store.dispatch(action);
-  };
-
   return (
     <StoreContext.Consumer>
-      {(store) => (
-        <MyPost
-          updateNewPostText={onPostChange}
-          addPost={addPost}
-          posts={store.getState().profilePage.posts}
-          newPostText={store.getState().profilePage.newPostText}
-        />
-      )}
+      {(store) => {
+        let state = props.store.getState();
+        let addPost = () => {
+          props.store.dispatch(addPostActionCreator());
+        };
+
+        let onPostChange = (text) => {
+          let action = updateNewPostTextActionCreator(text);
+          props.store.dispatch(action);
+        };
+
+        return (
+          <MyPost
+            updateNewPostText={onPostChange}
+            addPost={addPost}
+            posts={state.profilePage.posts}
+            newPostText={state.profilePage.newPostText}
+          />
+        );
+      }}
     </StoreContext.Consumer>
   );
 };
