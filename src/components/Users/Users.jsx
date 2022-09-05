c71906d88bce5dc894682a8a96075f43c4bcec40
andrew <andrewmoren93@gmail.com>
2022-09-05 20:02:05 +0300

    add onPageChanged

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -18,6 +18,17 @@ class Users extends React.Component {
       });
   }
 
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
   render() {
     let pagesCount = Math.ceil(
       this.props.totalUsersCount / this.props.pageSize
@@ -33,8 +44,8 @@ class Users extends React.Component {
             return (
               <span
                 className={this.props.currentPage === p && styles.selectedPage}
-                onClick={() => {
-                  this.props.setCurrentPage(p);
+                onClick={(e) => {
+                  this.onPageChanged();
                 }}
               >
                 {p}
