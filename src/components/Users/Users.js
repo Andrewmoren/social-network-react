fd4094bc99f17ffa855e8ce8c98c78a9a177778f
andrew <andrewmoren93@gmail.com>
2022-09-06 11:26:25 +0300

    add UserAPIComponent & container: Users

---
+++ src/components/Users/Users.js
@@ -0,0 +1,73 @@
+import styles from "./users.module.css";
+import userPhoto from "../../images/user.png";
+
+let Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+    if (i == 20) break;
+  }
+
+  return (
+    <div>
+      <div>
+        {pages.map((p) => {
+          return (
+            <span
+              className={props.currentPage === p && styles.selectedPage}
+              onClick={(e) => {
+                onPageChanged(p);
+              }}
+            >
+              {p}
+            </span>
+          );
+        })}
+      </div>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img
+                src={u.photos.small != null ? u.photo.small : userPhoto}
+                className={styles.userPhoto}
+              />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
