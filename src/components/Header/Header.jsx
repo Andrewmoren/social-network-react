d27b0a50f93bc85ffcdcae71eac2b39cfb6ebd29
andrew <andrewmoren93@gmail.com>
2022-09-10 12:21:05 +0300

    create auth login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
-import mod from "./Header.module.css"
+import { NavLink } from "react-router-dom";
+import mod from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <header className={mod.header}>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Facebook_Messenger_logo_2020.svg/1200px-Facebook_Messenger_logo_2020.svg.png" />
-      </header>
-    )
-}
+  return (
+    <header className={mod.header}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Facebook_Messenger_logo_2020.svg/1200px-Facebook_Messenger_logo_2020.svg.png" />
+      <div className={mod.loginBlock}>
+        <NavLink to="/login">Login</NavLink>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
