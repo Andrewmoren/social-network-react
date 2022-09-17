6e812d9fb65b593549feba58574052beffa43a11
andrew <andrewmoren93@gmail.com>
2022-09-17 22:27:46 +0300

    add asios:create

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,11 +1,16 @@
 import * as axios from "axios";
 
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  header: {
+    "API-KEY": "2fd61b36-0720-41b2-8251-f557940cabd2",
+  },
+});
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-  return axios
-    .get(
-      `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-      { withCredentials: true }
-    )
+  return instance
+    .get(`users?page=${currentPage}&count=${pageSize}`, {})
     .then((response) => {
       return response.data;
     });
