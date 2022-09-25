cc9dd468c9b1f6df970223ea355877c7af35d406
andrew <andrewmoren93@gmail.com>
2022-09-25 13:36:46 +0300

    create & add getUserProfile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Profile from "./Profile";
 import * as axios from "axios";
 import { connect } from "react-redux";
-import { setUserProfile } from "../../redux/profile-reducer";
+import { getUserProfile } from "../../redux/profile-reducer";
 import { useParams } from "react-router-dom";
-import { userAPI } from "../../redux/api/api";
+// import { userAPI } from "../../redux/api/api";
+
 export function withRouter(Children) {
   return (props) => {
     const match = { params: useParams() };
@@ -18,9 +19,7 @@ class ProfileContainer extends React.Component {
     if (!userId) {
       userId = 2;
     }
-    userAPI.getProfile(userId).then((response) => {
-      this.props.setUserProfile(response.data);
-    });
+    this.props.getUserProfile(userId);
   }
 
   render() {
@@ -33,6 +32,6 @@ let mapStateToProps = (state) => ({
 });
 
 let withUrlDataContainerComponent = withRouter(ProfileContainer);
-export default connect(mapStateToProps, { setUserProfile })(
+export default connect(mapStateToProps, { getUserProfile })(
   withUrlDataContainerComponent
 );
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,3 +1,5 @@
+import { userAPI } from "./api/api";
+
 const ADD_POST = "ADD_POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE_NEW_POST_TEXT";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
@@ -56,6 +58,11 @@ export const setUserProfile = (profile) => {
     type: SET_USER_PROFILE,
     profile,
   };
+};
+export const getUserProfile = (userId) => (dispatch) => {
+  userAPI.getProfile(userId).then((response) => {
+    dispatch(setUserProfile(response.data));
+  });
 };
 
 export default profileReducer;
